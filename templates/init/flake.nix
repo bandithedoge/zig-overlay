2b22d1a1225b2b95ba4cfdfcc5e8e26ee69b4dc0
bandithedoge <bandithedoge@protonmail.com>
2025-11-14 22:44:00 +0100

    simplify flake template

--- templates/init/flake.nix
+++ templates/init/flake.nix
@@ -3,44 +3,32 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     zig.url = "github:bandithedoge/zig-overlay";
-
-    # Used for shell.nix
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  } @ inputs: let
-    overlays = [
-      # Other overlays
-      (final: prev: {
-        zigpkgs = inputs.zig.packages.${prev.system};
-      })
-    ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }@inputs:
 
-    # Our supported systems are the same supported systems as the Zig binaries
-    systems = builtins.attrNames inputs.zig.packages;
-  in
-    flake-utils.lib.eachSystem systems (
-      system: let
-        pkgs = import nixpkgs {inherit overlays system;};
-      in rec {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            zigpkgs.master
-          ];
-        };
-
-        # For compatibility with older versions of the `nix` binary
-        devShell = self.devShells.${system}.default;
-      }
-    );
+    let
+      forAllSystems =
+        fn:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ] (system: fn nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells.default = forAllSystems (
+        pkgs:
+        pkgs.mkShell {
+          packages = [ ];
+        }
+      );
+    };
 }
